78de232fa58ca629a5b54b1037ffb1aaa25c09cd
jordannroussel <jroussel@jahia.com>
2016-11-25 12:01:39 +0100

    Image resizing : add rule for thumbnail creation + filter for rewriting img's src

---
+++ src/main/java/org/jahia/modules/academy/filters/AcademyImageUrlRewriter.java
@@ -0,0 +1,58 @@
+package org.jahia.modules.academy.filters;
+
+import net.htmlparser.jericho.OutputDocument;
+import net.htmlparser.jericho.Source;
+import net.htmlparser.jericho.StartTag;
+import org.jahia.services.render.RenderContext;
+import org.jahia.services.render.Resource;
+import org.jahia.services.render.filter.AbstractFilter;
+import org.jahia.services.render.filter.RenderChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Find HTML img tag with src towards internal images (/files/*), and rewrite the src so it points toward the thumbnail
+ */
+public class AcademyImageUrlRewriter extends AbstractFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(AcademyImageUrlRewriter.class);
+
+	private static final String PATH_TOWARD_IMG = "/files";
+
+	private static final String THUMBNAIL_NAME = "community750";
+
+	@Override
+	public String execute(String previousOut, RenderContext renderContext, Resource resource, RenderChain chain) throws Exception {
+		Source source = new Source(previousOut);
+		OutputDocument out = new OutputDocument(source);
+
+		List<StartTag> imgTags = source.getAllStartTags("img");
+		for (StartTag imgTag : imgTags) {
+			String srcValue = imgTag.getAttributeValue("src");
+			if (srcValue.startsWith(PATH_TOWARD_IMG)) {
+				logger.debug("SRC value to rewrite : " + srcValue);
+				StringBuilder sbSrcValue = new StringBuilder(srcValue);
+				if (srcValue.contains("?")) {
+					sbSrcValue.append("&");
+				}
+				else {
+					sbSrcValue.append("?");
+				}
+				sbSrcValue.append("t=" + THUMBNAIL_NAME);
+				logger.debug("New SRC value : " + sbSrcValue.toString());
+
+				// Rewrite the whole IMG tag
+				StringBuilder newImgTag = new StringBuilder("<img ");
+				newImgTag.append("alt=\"" + imgTag.getAttributeValue("alt") + "\"");
+				newImgTag.append(" src=\"" + sbSrcValue.toString() + "\"/>");
+
+				logger.debug("New IMG tag : " + newImgTag.toString());
+				out.replace(imgTag, newImgTag.toString());
+			}
+		}
+
+		return out.toString();
+	}
+}
