af1f575c1accb9e096cbc6fd8ccf37690342351f
Philippe Vollenweider <pvollenweider@jahia.com>
2017-09-12 11:55:03 +0200

    Minor fix on set-boost script

--- src/main/groovy/set-boost.groovy
+++ src/main/groovy/set-boost.groovy
@@ -70,7 +70,7 @@ for (Locale locale : site.getLanguagesAsLocales()) {
             }
             if (CollectionUtils.isNotEmpty(nodesToAutoPublish)) {
                 if (doIt) {
-                    JCRPublicationService.getInstance().publish(nodesToAutoPublish.asList(), Constants.EDIT_WORKSPACE, Constants.LIVE_WORKSPACE, false, "Change boost value")
+                    JCRPublicationService.getInstance().publish(nodesToAutoPublish.asList(), Constants.EDIT_WORKSPACE, Constants.LIVE_WORKSPACE);
                 };
                 logger.info("");
                 logger.info("Nodes which where republished:")
