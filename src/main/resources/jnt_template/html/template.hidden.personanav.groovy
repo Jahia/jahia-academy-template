e562f2f8b467aed14a57c4ce0370080d56446d20
Philippe Vollenweider <pvollenweider@jahia.com>
2025-05-23 18:21:02 +0200

    JAHIACOM-1412 Refactor persona navigation menu to generate context-aware links based on product, version, and persona path

--- src/main/resources/jnt_template/html/template.hidden.personanav.groovy
+++ src/main/resources/jnt_template/html/template.hidden.personanav.groovy
@@ -1,3 +1,4 @@
+import org.jahia.services.content.JCRSessionWrapper
 import org.jahia.utils.JcrUtils
 import javax.jcr.ItemNotFoundException
 import org.jahia.services.content.JCRContentUtils
@@ -65,64 +66,85 @@ def printPersona(JCRNodeWrapper startNode) {
     }
 }
 
-def printProducts(JCRNodeWrapper startNode, int level) {
+def printProducts(JCRNodeWrapper startNode, int level, JCRSessionWrapper session, String relativePath, String product, String version, String persona) {
     if (!startNode) return
 
     def children = JCRContentUtils.getChildrenOfType(startNode, "jmix:navMenuItem")
     int idx = 0
 
     children.eachWithIndex { menuItem, index ->
-        if (isCorrectType(menuItem) && (level > 0 || menuItem.isNodeType("jacademix:isProduct"))) {
-            if (menuItem.isNodeType("jacademix:isProduct")) idx++
-
-            String menuItemUrl = null
-            String menuItemTitle = menuItem.displayableName
-            boolean isCurrentProductPage = renderContext.mainResource.node.path.contains(menuItem.path) || level > 0
-
-            if (isCurrentProductPage) {
-                String extraClass = level == 0 ? " product" : ""
-
-                if (menuItem.isNodeType("jnt:page")) {
-                    menuItemUrl = renderContext.getResponse().encodeURL(menuItem.url)
-                } else if (menuItem.isNodeType("jnt:navMenuText")) {
-                    menuItemUrl = Functions.findFirstSubPageUrl(menuItem)
-                }
-
-                if (idx > 1 && level == 0) {
-                    print """
-                    <li><hr class="dropdown-divider"></li>
-                    """
-                }
-
-                print """
-                <li><a class="dropdown-item ${extraClass}" href="${menuItemUrl}">${menuItemTitle}</a></li>
-                """
-
-                if (level == 0 && !menuItem.isNodeType("jacademix:isVersionPage")) {
-                    printProducts(menuItem, level + 1)
-                }
+        String candidatePath = null
+        JCRNodeWrapper candidateNode = null;
+        String menuItemTitle = menuItem.displayableName
+        // we iterate the versions. So we try to get the new path based on the current version
+        try {
+            candidatePath = "/sites/academy/home/documentation/" + product + "/" + menuItem.getName() + "/" + persona + relativePath;
+            candidateNode = currentResource.getSession().getNode(candidatePath)
+            menuItemUrl = candidateNode.getUrl();
+        } catch  (javax.jcr.PathNotFoundException e) {
+            // previous candidatePath id not work... we try to generate path to the persona now
+            try {
+                candidatePath = "/sites/academy/home/documentation/" + product + "/" + menuItem.getName() + "/" + persona;
+                candidateNode = currentResource.getSession().getNode(candidatePath)
+                menuItemUrl = Functions.findFirstSubPageUrl(candidateNode)
+            } catch (javax.jcr.PathNotFoundException e2) {
+                menuItemUrl = Functions.findFirstSubPageUrl(menuItem)
             }
         }
+
+        print """
+        <li><a class="dropdown-item" href="${menuItemUrl}">${menuItemTitle}</a></li>
+        """
     }
 }
 
 
 def startNode = null
+String documentationPath = "/sites/academy/home/documentation";
 JCRNodeWrapper curentPageNode = renderContext.mainResource.node
-JCRNodeWrapper documentationNode = currentResource.getSession().getNode("/sites/academy/home/documentation")
-
+JCRNodeWrapper documentationNode = currentResource.getSession().getNode(documentationPath)
 List<JCRNodeWrapper> parentPages = JCRTagUtils.getParentsOfType(curentPageNode, "jmix:navMenuItem")
+
+// documentationPath / product / version / persona (has mixin jacademix:isVersionPage) / relativePath
+
+String persona = null
+String version = null
+String product = null
+
+JCRNodeWrapper personaNode = null
+JCRNodeWrapper versionNode = null
+JCRNodeWrapper productNode = null
+
+String relativePath = null // right part after persona
 
 if (!parentPages.empty) {
     List<JCRNodeWrapper> reversedList = parentPages.reverse()
 
     for (JCRNodeWrapper parentPage : reversedList) {
         if (JCRTagUtils.isNodeType(parentPage, "jacademix:isVersionPage")) {
+            personaNode = parentPage;
+            persona = personaNode.getName();
+
+            versionNode = personaNode.getParent()
+            version = versionNode.getName()
+
             startNode = parentPage.getParent()
+
+            relativePath = curentPageNode.getPath().replace(personaNode.getPath(), "");
+
+            break
+        }
+    }
+
+    for (JCRNodeWrapper parentPage : reversedList) {
+        if (JCRTagUtils.isNodeType(parentPage, "jacademix:isProduct")) {
+            productNode = parentPage;
+            product = productNode.getName()
             break
         }
     }
 }
+
 
 if (startNode) {
     if (JCRContentUtils.getChildrenOfType(startNode, "jacademix:isVersionPage").size() > 1) {
@@ -168,8 +190,8 @@ if (startNode) {
                     </button>
                     <ul class="dropdown-menu">
             """
-            if (documentationNode != null) {
-                printProducts(documentationNode, 0)
+            if (productNode != null) {
+                printProducts(productNode, 0, currentResource.getSession(), relativePath, product, version, persona);
             }
             print """
                     </ul>
