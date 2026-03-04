7d71cece068d465a74d9bd9613b7a8ab2433cf95
Philippe Vollenweider <pvollenweider@jahia.com>
2026-03-04 09:33:05 +0100

    JAHIACOM-1501 Prevent duplicate rendering in sidenav for level >= 3 Remove extra nested <li> that re-printed the same menu item inside its own children list.

--- src/main/resources/jnt_template/html/template.hidden.sidenav.groovy
+++ src/main/resources/jnt_template/html/template.hidden.sidenav.groovy
@@ -1,7 +1,6 @@
 import javax.jcr.ItemNotFoundException
 import org.jahia.services.content.JCRContentUtils
 import org.jahia.services.content.JCRNodeWrapper
-import org.jahia.services.render.RenderService
 import org.jahia.services.render.Resource
 import org.jahia.taglibs.jcr.node.JCRTagUtils
 import org.slf4j.LoggerFactory
@@ -9,149 +8,187 @@ import org.jahia.modules.academy.Functions
 
 logger = LoggerFactory.getLogger(this.class)
 
-def printMenu;
+def printMenu
 printMenu = { startNode, level, maxlevel ->
-    if (startNode != null) {
-        children = JCRContentUtils.getChildrenOfType(startNode, "jmix:navMenuItem")
-        children.eachWithIndex() { menuItem, index ->
-            if (menuItem != null) {
-                def correctType = true;
-                if (menuItem.isNodeType("jmix:navMenu")) {
-                    correctType = false;
+    if (startNode == null) {
+        return
+    }
+
+    def children = JCRContentUtils.getChildrenOfType(startNode, "jmix:navMenuItem")
+    children.eachWithIndex { JCRNodeWrapper menuItem, int index ->
+        if (menuItem == null) {
+            return
+        }
+
+        def correctType = true
+        if (menuItem.isNodeType("jmix:navMenu")) {
+            correctType = false
+        }
+
+        if (menuItem.properties['j:displayInMenuName']) {
+            correctType = false
+            menuItem.properties['j:displayInMenuName'].each {
+                correctType |= (it.string.equals(currentNode.name))
+            }
+        }
+
+        if (!correctType) {
+            return
+        }
+
+        boolean isNavText = menuItem.isNodeType('jnt:navMenuText')
+        boolean hasChildren = (level < maxlevel) && JCRTagUtils.hasChildrenOfType(menuItem, "jmix:navMenuItem")
+
+        String menuItemUrl = null
+        String menuItemId = "page-" + menuItem.getIdentifier()
+        String menuItemTitle = menuItem.displayableName
+
+        boolean isActive = renderContext.mainResource.node.path.indexOf(menuItem.path) > -1
+        boolean isCurrent = renderContext.mainResource.node.path.equals(menuItem.path)
+        String statusClass = isCurrent ? ' active' : isActive ? ' inpath' : ''
+
+        // Resolve URL + title depending on type
+        if (menuItem.isNodeType('jnt:page')) {
+            menuItemUrl = renderContext.getResponse().encodeURL(menuItem.url)
+
+        } else if (menuItem.isNodeType('jnt:nodeLink')) {
+            JCRNodeWrapper refNode = menuItem.properties['j:node']?.node
+            if (refNode != null) {
+                isActive = renderContext.mainResource.node.path.indexOf(refNode.path) > -1
+                isCurrent = renderContext.mainResource.node.path.equals(refNode.path)
+                statusClass = isCurrent ? ' active' : isActive ? ' inpath' : ''
+
+                currentResource.dependencies.add(refNode.getCanonicalPath())
+
+                menuItemTitle = menuItem.getPropertyAsString("jcr:title")
+                if (menuItemTitle == null || "".equals(menuItemTitle)) {
+                    menuItemTitle = refNode.displayableName
                 }
+                menuItemUrl = renderContext.getResponse().encodeURL(refNode.url)
+            }
 
-                if (menuItem.properties['j:displayInMenuName']) {
-                    correctType = false;
-                    menuItem.properties['j:displayInMenuName'].each() {
-                        correctType |= (it.string.equals(currentNode.name))
-                    }
-                }
+        } else if (menuItem.isNodeType('jnt:externalLink')) {
+            menuItemUrl = menuItem.properties['j:url']?.string
 
-                if (correctType) {
-                    boolean hasChildren = level < maxlevel && JCRTagUtils.hasChildrenOfType(menuItem, "jmix:navMenuItem")
-                    String menuItemUrl = null;
-                    String menuItemId = "page-" + menuItem.getIdentifier();
-                    String menuItemTitle = menuItem.displayableName;
-                    boolean isActive = renderContext.mainResource.node.path.indexOf(menuItem.path) > -1;
-                    boolean isCurrent = renderContext.mainResource.node.path.equals(menuItem.path);
-                    String statusClass = isCurrent ? ' active' : isActive ? ' inpath' : '';
+        } else if (isNavText) {
+            // Recommended improvement: keep it as a label (no URL fallback)
+            menuItemUrl = null
 
-                    if (menuItem.isNodeType('jnt:page')) {
-                        menuItemUrl = renderContext.getResponse().encodeURL(menuItem.url);
-                    } else if (menuItem.isNodeType('jnt:nodeLink')) {
-                        JCRNodeWrapper refNode = menuItem.properties['j:node'].node;
-                        if (refNode != null) {
-                            isActive = renderContext.mainResource.node.path.indexOf(refNode.path) > -1;
-                            isCurrent = renderContext.mainResource.node.path.equals(refNode.path);
-                            statusClass = isCurrent ? ' active' : isActive ? ' inpath' : '';
-                            currentResource.dependencies.add(refNode.getCanonicalPath());
-                            menuItemTitle = menuItem.getPropertyAsString("jcr:title");
-                            if (menuItemTitle == null || "".equals(menuItemTitle)) {
-                                menuItemTitle = refNode.displayableName;
-                            }
-                            menuItemUrl = renderContext.getResponse().encodeURL(refNode.url);
-                        }
-                    } else if (menuItem.isNodeType('jnt:externalLink')) {
-                        menuItemUrl = menuItem.properties['j:url'].string;
-                    }
-                    if (menuItemUrl == null || "".equals(menuItemUrl)) {
-                        menuItemUrl = Functions.findFirstSubPageUrl(menuItem);
-                    }
+        } else {
+            // Optional fallback for other node types
+            menuItemUrl = Functions.findFirstSubPageUrl(menuItem)
+        }
 
-                    if (hasChildren && level < maxlevel) {
-                        // FIXME: We don't manage when the element is a menu-label
-                        if (level == 1) {
-                            print "<li>";
-                                print "<a class='jac-secondary-navigation-link jac-secondary-navigation-link_one has-children d-inline-flex align-items-center rounded collapsed' href='${menuItemUrl}' id='${menuItemId}'>${menuItemTitle}"
-                                    if (isCurrent) {
-                                        print " <span class='visually-hidden'>(current)</span>";
-                                    }
-                                print "</a>";
-                                print "<ul>";
-                                    printMenu(menuItem, level + 1, maxlevel);
-                                print "</ul>";
-                            print "</li>";
+        // Helpers: render label/link + current marker
+        def renderTitle = { ->
+            print "${menuItemTitle}"
+            if (isCurrent) {
+                print " <span class='visually-hidden'>(current)</span>"
+            }
+        }
 
-                        } else if (level == 2) {
-                            print "<li>";
-                                print "<span class='jac-secondary-navigation-link jac-secondary-navigation-link_two has-children btn d-inline-flex align-items-center rounded collapse' data-bs-target='#sideMenu-${currentNode.identifier}-${menuItem.identifier}' data-bs-toggle='collapse' aria-expanded='false'>${menuItemTitle}"
-                                    if (isCurrent) {
-                                        print " <span class='visually-hidden'>(current)</span>";
-                                    }
-                                print "</span>";
-                                print "<div class=\"collapse${isActive?'d':''}\" id=\"sideMenu-${currentNode.identifier}-${menuItem.identifier}\">"
-                                    print "<ul class=\"fw-normal pb-1 small\">";
-                                        printMenu(menuItem, level + 1, maxlevel);
-                                    print "</ul>";
-                                print "</div>"
-                            print "</li>";
+        def renderLinkOrLabel = { String cssClasses, String extraAttrs = "" ->
+            if (isNavText || menuItemUrl == null || "".equals(menuItemUrl)) {
+                print "<span class=\"${cssClasses}\" id='${menuItemId}' ${extraAttrs}>"
+                renderTitle()
+                print "</span>"
+            } else {
+                print "<a class=\"${cssClasses}\" href=\"${menuItemUrl}\" id='${menuItemId}' ${extraAttrs}>"
+                renderTitle()
+                print "</a>"
+            }
+        }
 
-                        } else {
-                            print "<li>";
-                                print "<a href='${menuItemUrl}' id='${menuItemId}'>${menuItemTitle}"
-                                    if (isCurrent) {
-                                        print " <span class='visually-hidden'>(current)</span>";
-                                    }
-                                print "</a>";
-                                print "<ul class=\"fw-normal pb-1 small\">";
-                                    print "<li><a href='${menuItemUrl}' id='${menuItemId}'>${menuItemTitle}</a></li>";
-                                    printMenu(menuItem, level + 1, maxlevel);
-                                print "</ul>";
-                            print "</li>";
-                        }
+        // Render
+        if (hasChildren && level < maxlevel) {
+            if (level == 1) {
+                print "<li>"
+                renderLinkOrLabel(
+                        "jac-secondary-navigation-link jac-secondary-navigation-link_one has-children d-inline-flex align-items-center rounded collapsed${statusClass}",
+                        ""
+                )
+                print "<ul>"
+                printMenu(menuItem, level + 1, maxlevel)
+                print "</ul>"
+                print "</li>"
 
-                    } else {
-                        if (level == 1) {
-                            print "<li>";
-                                print "<a class=\"jac-secondary-navigation-link jac-secondary-navigation-link_one d-inline-flex align-items-center rounded ${isCurrent ? ' active' : ''}\" href=\"${menuItemUrl}\" id='${menuItemId}'>${menuItemTitle}";
-                                    if (isCurrent) {
-                                        print " <span class=\"visually-hidden\">(current)</span>";
-                                    }
-                                print "</a>"
-                            print "</li>";
+            } else if (level == 2) {
+                print "<li>"
+                // Here the existing template uses a <span> with collapse behavior; keep that
+                // Also: navMenuText is naturally fine as a span.
+                print "<span class='jac-secondary-navigation-link jac-secondary-navigation-link_two has-children btn d-inline-flex align-items-center rounded collapse${statusClass}' " +
+                        "data-bs-target='#sideMenu-${currentNode.identifier}-${menuItem.identifier}' data-bs-toggle='collapse' aria-expanded='false'>"
+                renderTitle()
+                print "</span>"
 
-                        } else if (level == 2) {
-                                print "<li>";
-                                    print "<a class=\"jac-secondary-navigation-link jac-secondary-navigation-link_two d-inline-flex align-items-center rounded ${isCurrent?' active':''}\" href=\"${menuItemUrl}\" id='${menuItemId}'>${menuItemTitle}";
-                                        if (isCurrent) {
-                                            print " <span class=\"visually-hidden\">(current)</span>";
-                                        }
-                                    print "</a>"
-                                print "</li>";
+                print "<div class=\"collapse${isActive ? 'd' : ''}\" id=\"sideMenu-${currentNode.identifier}-${menuItem.identifier}\">"
+                print "<ul class=\"fw-normal pb-1 small\">"
+                printMenu(menuItem, level + 1, maxlevel)
+                print "</ul>"
+                print "</div>"
+                print "</li>"
 
-                        } else {
-                            print "<li>";
-                                print "<a class=\"jac-secondary-navigation-link jac-secondary-navigation-link_three d-inline-flex align-items-center rounded ${isCurrent ? ' active' : ''}\" href=\"${menuItemUrl}\" id='${menuItemId}'>${menuItemTitle}";
-                                    if (isCurrent) {
-                                        print " <span class=\"visually-hidden\">(current)</span>";
-                                    }
-                                print "</a>"
-                            print "</li>";
-                        }
-                    }
-                }
+            } else {
+                // IMPORTANT FIX: Do NOT print the same item again inside its own <ul>
+                print "<li>"
+                renderLinkOrLabel(
+                        "jac-secondary-navigation-link jac-secondary-navigation-link_three has-children d-inline-flex align-items-center rounded collapsed${statusClass}",
+                        ""
+                )
+                print "<ul class=\"fw-normal pb-1 small\">"
+                printMenu(menuItem, level + 1, maxlevel)
+                print "</ul>"
+                print "</li>"
+            }
+
+        } else {
+            if (level == 1) {
+                print "<li>"
+                renderLinkOrLabel(
+                        "jac-secondary-navigation-link jac-secondary-navigation-link_one d-inline-flex align-items-center rounded${isCurrent ? ' active' : ''}",
+                        ""
+                )
+                print "</li>"
+
+            } else if (level == 2) {
+                print "<li>"
+                renderLinkOrLabel(
+                        "jac-secondary-navigation-link jac-secondary-navigation-link_two d-inline-flex align-items-center rounded${isCurrent ? ' active' : ''}",
+                        ""
+                )
+                print "</li>"
+
+            } else {
+                print "<li>"
+                renderLinkOrLabel(
+                        "jac-secondary-navigation-link jac-secondary-navigation-link_three d-inline-flex align-items-center rounded${isCurrent ? ' active' : ''}",
+                        ""
+                )
+                print "</li>"
             }
         }
     }
 }
 
-long maxlevel = 5;
+long maxlevel = 5
 
-JCRNodeWrapper startNode = null;
-String productName = "";
-JCRNodeWrapper curentPageNode = renderContext.mainResource.node;
-List<JCRNodeWrapper> parentPages = JCRTagUtils.getParentsOfType(curentPageNode,"jmix:navMenuItem");
+JCRNodeWrapper startNode = null
+String productName = ""
+JCRNodeWrapper curentPageNode = renderContext.mainResource.node
+
+List<JCRNodeWrapper> parentPages = JCRTagUtils.getParentsOfType(curentPageNode, "jmix:navMenuItem")
 if (!parentPages.empty) {
-    List<JCRNodeWrapper> reversedList = new ArrayList();
-    reversedList.addAll(parentPages);
-    Collections.reverse(reversedList);
+    List<JCRNodeWrapper> reversedList = new ArrayList()
+    reversedList.addAll(parentPages)
+    Collections.reverse(reversedList)
+
     for (JCRNodeWrapper parentPage : reversedList) {
         if (JCRTagUtils.isNodeType(parentPage, "jacademix:isProduct")) {
-            productName = parentPage.getDisplayableName();
+            productName = parentPage.getDisplayableName()
         }
-        if (JCRTagUtils.isNodeType(parentPage,'jacademix:isVersionPage')) {
-            startNode = parentPage;
-            break;
+        if (JCRTagUtils.isNodeType(parentPage, 'jacademix:isVersionPage')) {
+            startNode = parentPage
+            break
         }
     }
 }
@@ -159,14 +196,15 @@ if (!parentPages.empty) {
 if (startNode != null) {
     // Add dependencies to parent of main resource so that we are aware of new pages at sibling level
     try {
-        currentResource.dependencies.add(renderContext.mainResource.node.getParent().getCanonicalPath());
+        currentResource.dependencies.add(renderContext.mainResource.node.getParent().getCanonicalPath())
     } catch (ItemNotFoundException e) {
+        // ignore
     }
 
-    print "<nav class=\"jac-secondary-navigation p-4 sticky-top\" id=\"bd-docs-nav\" aria-label=\"Docs navigation\">";
-        print "<p class='h4'>$productName</p>";
-        print "<ul class=\"jac-secondary-navigation-list mb-0\">";
-            printMenu(startNode, 1,  maxlevel)
-        print "</ul>"
-    print "</nav>";
+    print "<nav class=\"jac-secondary-navigation p-4 sticky-top\" id=\"bd-docs-nav\" aria-label=\"Docs navigation\">"
+    print "<p class='h4'>${productName}</p>"
+    print "<ul class=\"jac-secondary-navigation-list mb-0\">"
+    printMenu(startNode, 1, maxlevel)
+    print "</ul>"
+    print "</nav>"
 }
